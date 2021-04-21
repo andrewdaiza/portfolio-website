67c3bc71829d08f9529d02a3f0f64228a0ad5a24
andrew.daiza <andrewdaizatech@gmail.com>
2021-04-21 14:51:14 -0700

    fixed console error

--- app.js
+++ app.js
@@ -5,29 +5,29 @@ window.onscroll = function () {
       document.body.scrollTop > 80 ||
       document.documentElement.scrollTop > 80
     ) {
-      $('#header').css('background', '#fff');
-      $('#header').css('color', '#000');
-      $('#header').css('box-shadow', '0px 0px 20px rgba(0,0,0,0.09)');
-      $('#header').css('padding', '4vh 4vw');
-      $('#navigation a').hover(
+      $("#header").css("background", "#fff");
+      $("#header").css("color", "#000");
+      $("#header").css("box-shadow", "0px 0px 20px rgba(0,0,0,0.09)");
+      $("#header").css("padding", "4vh 4vw");
+      $("#navigation a").hover(
         function () {
-          $(this).css('border-bottom', '2px solid rgb(255, 44, 90)');
+          $(this).css("border-bottom", "2px solid rgb(255, 44, 90)");
         },
         function () {
-          $(this).css('border-bottom', '2px solid transparent');
+          $(this).css("border-bottom", "2px solid transparent");
         }
       );
     } else {
-      $('#header').css('background', 'transparent');
-      $('#header').css('color', '#fff');
-      $('#header').css('box-shadow', '0px 0px 0px rgba(0,0,0,0)');
-      $('#header').css('padding', '6vh 4vw');
-      $('#navigation a').hover(
+      $("#header").css("background", "transparent");
+      $("#header").css("color", "#fff");
+      $("#header").css("box-shadow", "0px 0px 0px rgba(0,0,0,0)");
+      $("#header").css("padding", "6vh 4vw");
+      $("#navigation a").hover(
         function () {
-          $(this).css('border-bottom', '2px solid #fff');
+          $(this).css("border-bottom", "2px solid #fff");
         },
         function () {
-          $(this).css('border-bottom', '2px solid transparent');
+          $(this).css("border-bottom", "2px solid transparent");
         }
       );
     }
@@ -36,11 +36,11 @@ window.onscroll = function () {
 
 /* Navigation scroll */
 $(document).ready(function () {
-  $('a').on('click', function (event) {
-    if (this.hash !== '') {
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
       event.preventDefault();
       var hash = this.hash;
-      $('body,html').animate(
+      $("body,html").animate(
         {
           scrollTop: $(hash).offset().top,
         },
